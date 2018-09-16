0bc1ab2bfa9147c00f7adee8a6a7750e0ad56fa5
Gianluca Campos <gianlucampos97@gmail.com>
2018-09-16 20:55:55 -0300

    Realizando fit do novo cronossomo
    Falta:
    -Definir novo_cronossomo como a próxima geração
    -Criar uma interface
    -Transformar etapas em funções (ou não)

--- index.py
+++ index.py
@@ -21,129 +21,144 @@ pm = 10  # taxa mutacao
 
 # 2
 n_populacao = 6  # nº população == nº cronossomos
-for inicio in range(n_geracoes):
-    cronossomo = []
-    fx = []
-    fit = []
-    total = 0
-    i = 0
+try:
+    for inicio in range(n_geracoes):
+        cronossomo = []
+        fx = []
+        fit = []
+        total = 0
+        i = 0
 
-    print("\n****************************************")
-    print("{}ª geração de cronossomos".format(inicio + 1))
-    print("****************************************")
+        print("\n****************************************")
+        print("{}ª geração de cronossomos".format(inicio + 1))
+        print("****************************************")
 
-    for i in range(n_cronossomos):
-        a = random.randint(0, 30)
-        b = random.randint(0, 30)
-        c = random.randint(0, 30)
-        d = random.randint(0, 30)
-        cronossomo.insert(i, str(a) + ";" + str(b) + ";" + str(c) + ";" + str(d))
+        for i in range(n_cronossomos):
+            a = random.randint(0, 30)
+            b = random.randint(0, 30)
+            c = random.randint(0, 30)
+            d = random.randint(0, 30)
+            cronossomo.insert(i, str(a) + ";" + str(b) + ";" + str(c) + ";" + str(d))
 
-        # 3 Executando etapas 4 a 7
+            # 3 Executando etapas 4 a 7
 
-        # 4 Avaliação
+            # 4 Avaliação
 
-        a, b, c, d = cronossomo[i].split(';')
-        a = int(a)
-        b = int(b)
-        c = int(c)
-        d = int(d)
-        fx.insert(i, abs((a + (2 * b) + (3 * c) + (4 * d)) - 30))
+            a, b, c, d = cronossomo[i].split(';')
+            a = int(a)
+            b = int(b)
+            c = int(c)
+            d = int(d)
+            fx.insert(i, abs((a + (2 * b) + (3 * c) + (4 * d)) - 30))
 
-        # Fitness
+            # Fitness
 
-        ft = round((1 / (1 + fx[i])), 4)
-        fit.insert(i, ft)
-        total = round((ft + total), 4)
+            ft = round((1 / (1 + fx[i])), 4)
+            fit.insert(i, ft)
+            total = round((ft + total), 4)
 
-    print("Cronossomos:")
-    print(cronossomo)
-    print("F(x):")
-    print(fx)
-    print("Fitness:")
-    print(fit)
-    print("Total:")
-    print(total)
+        print("Cronossomos:")
+        print(cronossomo)
+        print("F(x):")
+        print(fx)
+        print("Fitness:")
+        print(fit)
+        print("Total:")
+        print(total)
 
-    # 5 Calculando Probabilidade (roulette)
+        # 5 Calculando Probabilidade (roulette)
 
-    p = []
-    for i in range(n_cronossomos):
-        p.insert(i, round((fit[i] / total), 4))
+        p = []
+        for i in range(n_cronossomos):
+            p.insert(i, round((fit[i] / total), 4))
 
-    print("Probabilidade P: ")
-    print(p)
+        print("Probabilidade P: ")
+        print(p)
 
-    ''' Não serve pra nada por isso deixar assim
-    # Probabilidade cumulativa 
-    c = []
-    acumula = 0
-    for i in range(n_cronossomos):
-        acumula += p[i]
-        c.insert(i, round(acumula, 4))
-    
-    print("Probabilidade acumulativa C:")
-    print(c)
-    '''
-    # 6 Crossover
-    print("\nCrossover:\n\nValores Aleatórios:")
-    # Seleção de cronossomos
-    r = []
-    cs = []  # cronossomos selecionados possui o indice dos cronossomos
-    for i in range(n_cronossomos):
-        r.insert(i, round(random.uniform(0, 1), 4))
-        print("R[{}]: {}".format(i, r[i]))
+        ''' Não serve pra nada por isso deixar assim
+        # Probabilidade cumulativa 
+        c = []
+        acumula = 0
+        for i in range(n_cronossomos):
+            acumula += p[i]
+            c.insert(i, round(acumula, 4))
+        
+        print("Probabilidade acumulativa C:")
+        print(c)
+        '''
+        # 6 Crossover
+        print("\nCrossover:\n\nValores Aleatórios:")
+        # Seleção de cronossomos
+        r = []
+        cs = []  # cronossomos selecionados possui o indice dos cronossomos
+        for i in range(n_cronossomos):
+            r.insert(i, round(random.uniform(0, 1), 4))
+            print("R[{}]: {}".format(i, r[i]))
 
-        if r[i] < pc:
-            cs.insert(i, i)
+            if r[i] < pc:
+                cs.insert(i, i)
 
-    # Fazer um loop while para repetir o processo inicial até que seja gerado novamente
-    if len(cs) <= 1:
-        print("Não possui valores suficientes para gerar crossover, Regerar valores ")
-    else:
-        print("Cronossomos selecionados Cs: ")
-        print(cs)
+        # Fazer um loop while para repetir o processo inicial até que seja gerado novamente
+        if len(cs) <= 1:
+            print("Não possui valores suficientes para gerar crossover, Regerar valores ")
+        else:
+            print("Cronossomos selecionados Cs: ")
+            print(cs)
 
-    # Determinar posição do crossover
-    novo_cronossomo = cronossomo[:]
-    if len(cs) > 1:
-        print("Cortando a partir do ")
-        for i in range(len(cs)):
-            va = random.randint(1, 3)
-            a, b, c, d = cronossomo[cs[i]].split(";")
-            if i == len(cs) - 1:
-                a0, b0, c0, d = cronossomo[cs[0]].split(";")
-            else:
-                a0, b0, c0, d = cronossomo[cs[i + 1]].split(";")
-            if va == 1:
-                b = b0
-                c = c0
-            if va == 2:
-                c = c0
-            novo_cronossomo[cs[i]] = str(a) + ";" + str(b) + ";" + str(c) + ";" + str(d)
-            print(str(va) + "º gene do cronossomo {}".format(cs[i]))
+        # Determinar posição do crossover
+        novo_cronossomo = cronossomo[:]
+        if len(cs) > 1:
+            print("Cortando a partir do ")
+            for i in range(len(cs)):
+                va = random.randint(1, 3)
+                a, b, c, d = cronossomo[cs[i]].split(";")
+                if i == len(cs) - 1:
+                    a0, b0, c0, d = cronossomo[cs[0]].split(";")
+                else:
+                    a0, b0, c0, d = cronossomo[cs[i + 1]].split(";")
+                if va == 1:
+                    b = b0
+                    c = c0
+                if va == 2:
+                    c = c0
+                novo_cronossomo[cs[i]] = str(a) + ";" + str(b) + ";" + str(c) + ";" + str(d)
+                print(str(va) + "º gene do cronossomo {}".format(cs[i]))
 
-        print("Cronossomo após crossover")
-        print(novo_cronossomo)
+            print("Cronossomo após crossover")
+            print(novo_cronossomo)
 
-        # 7 Mutação
+            # 7 Mutação
 
-        print("\nMutação:\n")
-        total_genes = 4 * n_populacao
-        total_mutacoes = round(total_genes * (pm / 100))
-        print("Total de genes a serem mutados: {}".format(total_mutacoes))
-        for i in range(total_mutacoes):
-            posicao_corte = random.randint(0, total_genes)
-            # print(posicao_corte)
-            pos_cronossomo = int(posicao_corte / 4)
-            pos_gene = posicao_corte % 4
-            vetor = novo_cronossomo[pos_cronossomo].split(";")
-            print("Mutação no {}º cronossomo".format(pos_cronossomo + 1))
-            vetor = novo_cronossomo[pos_cronossomo].split(";")
-            print("{}º gene".format(pos_gene + 1))
-            vetor[pos_gene] = str(random.randint(0, 30))
-            novo_cronossomo[pos_cronossomo] = vetor[0] + ";" + vetor[1] + ";" + vetor[2] + ";" + vetor[3]
-        print("Cronossomo após mutação")
-        print(novo_cronossomo)
-        # cronossomo = novo_cronossomo[:], pois ele será a nova geração
-        # Fazer fit do novo_cronossomo[:]
+            print("\nMutação:\n")
+            total_genes = 4 * n_populacao
+            total_mutacoes = round(total_genes * (pm / 100))
+            print("Total de genes a serem mutados: {}".format(total_mutacoes))
+            for i in range(total_mutacoes):
+                posicao_corte = random.randint(0, total_genes)
+                pos_cronossomo = int(posicao_corte / 4)
+                pos_gene = posicao_corte % 4
+                vetor = novo_cronossomo[pos_cronossomo].split(";")
+                print("Mutação no {}º cronossomo".format(pos_cronossomo + 1))
+                print("{}º gene".format(pos_gene + 1))
+                vetor[pos_gene] = str(random.randint(0, 30))
+                novo_cronossomo[pos_cronossomo] = vetor[0] + ";" + vetor[1] + ";" + vetor[2] + ";" + vetor[3]
+            print("Cronossomo após mutação")
+            print(novo_cronossomo)
+            print("")
+            # cronossomo = novo_cronossomo[:], pois ele será a nova geração
+            nova_fx = []
+            for i in range(n_cronossomos):
+                a, b, c, d = novo_cronossomo[i].split(';')
+                a = int(a)
+                b = int(b)
+                c = int(c)
+                d = int(d)
+                # print("Nova F(x)[{}]: {}".format(i, abs((a + (2 * b) + (3 * c) + (4 * d)) - 30)))
+                nova_fx.insert(i, abs((a + (2 * b) + (3 * c) + (4 * d)) - 30))
+            print("Nova F(x):")
+            print(nova_fx)
+except Exception:
+    print("\n" * 100)
+    print("\n*************************************")
+    print("FERROU JAMELAO, TENTE NOVAMENTE -\o/-")
+    print("*************************************")
